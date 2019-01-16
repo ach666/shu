8d159af6f8459a18c935f24a16fe214c684073df
ach666 <46721480+ach666@users.noreply.github.com>
2019-01-15 19:18:32 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ async def on_ready():
 async def on_message(message):
     if message.author != client.user:
       if ("nazuna" in message.content.lower()) or ("nito" in message.content.lower()):
-          await client.send_message(message.channel,"NITO " * random.randint(14,39))
+          await client.send_message(message.channel,"NITO " * random.randint(14,139))
 
       elif ("pussy" in message.content.lower()):
         if (message.author.bot):return
