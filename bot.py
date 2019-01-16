c0ddf6a50dd2dcc8a530d3be5e629a121f609613
ach666 <46721480+ach666@users.noreply.github.com>
2019-01-15 19:16:36 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -125,5 +125,5 @@ Asahi ga kimi o torimodosu made wa bokutachi no jikan""")
       elif ("burger" in message.content.lower()):
         await client.send_file(message.channel,"BURGER.png")
 
-token = os.environ("DISCORD_BOT_SECRET")
+token = os.environ.get("DISCORD_BOT_SECRET")
 client.run(token)
